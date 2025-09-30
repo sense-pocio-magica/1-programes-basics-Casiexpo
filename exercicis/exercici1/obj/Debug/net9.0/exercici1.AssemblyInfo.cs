f0e35770b92889400757fed79b6256b3f6b7ab9e
Casiexpo <casiexpo10@gmail.com>
2025-09-30 16:03:10 +0200

    Exercici_1

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033395b090265ceecdda678dd9c7d01c3b25de61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441c3eb31725e977208f3ef1b1db3c41c2d2f0ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
