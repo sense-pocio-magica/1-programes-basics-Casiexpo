eddf5907fe68b9eae4ac2c0d5111c36d5a763a65
Casiexpo <casiexpo10@gmail.com>
2025-10-01 18:52:30 +0200

    NS-que demana pujar

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aec32e5800f256902f3ff18551e41ad960ec9f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9daea598c1cb704898d7609ecda5b271abd72f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
